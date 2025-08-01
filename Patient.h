9bbd312a6060e1b7fbc4d16a361919f5df7ccb55
Himank2004 <159522521+Himank2004@users.noreply.github.com>
2025-08-01 21:44:24 +0530

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -1,17 +1,18 @@
 #pragma once
 #include "Person.h"
+using namespace std;
 
 class Patient : public Person {
 private:
     int patientId;
-    std::string condition;
+    string condition;
 
 public:
-    Patient(std::string name, int age, std::string gender, int id, std::string condition);
+    Patient(string name, int age, string gender, int id, string condition);
 
     void displayInfo() const override;
 
     // Specific getters
     int getPatientId() const;
-    std::string getCondition() const;
+    string getCondition() const;
 };
